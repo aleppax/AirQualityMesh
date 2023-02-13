e55ab648a74ff752df7a1c7cc465b8902ca0694f
Alessandro Proglio <ale.proglio@gmail.com>
2023-02-13 11:50:33 +0100

    adopted SPS30 library

--- softwarePico/main.py
+++ softwarePico/main.py
@@ -1,11 +1,12 @@
-from libs import config, cron, leadacid, logger, picosngcja5, wlan
+from libs import config, cron, leadacid, logger, picosngcja5, sps30, wlan,
 
 # init logger
 logger.info('booting')
 #init I2C and GPIO. access a port with gpio['GP2']
 i2c, gpio = config.initialize_board()
 #init pm sensor
-#pm_sensor = picosngcja5.SNGCJA5(i2c)
+#pm_sensor_p = picosngcja5.SNGCJA5(i2c) # Panasonic SNGCJA5
+#pm_sensor_s = sps30.SPS30()
 # init network
 wlan.initialize()
 # init RTC
