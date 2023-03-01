c76ee5842830be9ddeb810b4d9bccda808a36981
Alessandro Proglio <ale.proglio@gmail.com>
2023-03-01 18:38:24 +0100

    fixed error when updating online a reboot is required

--- softwarePico/version.py
+++ softwarePico/version.py
@@ -1,15 +1,15 @@
-version = 3
+version = 5
 folders = ['/', '/libs', '/html', '/logs']
 #updates or new files. this file is manually updated and can only add or modify files or add folders
 updated_files = {
-    '/'     : ['main.py',], # there is no need to include version.py
-    '/libs' : ['datalogger.py','config.py',],
+    '/'     : [], # there is no need to include version.py
+    '/libs' : ['cron.py'],
     '/html' : [], 
 }
 # full update requires a list of all files
 all_files = {
     '/'     : ['main.py',], # there is no need to include version.py
-    '/libs' : ['ahtx0.py','config.py','__init__.py','logger.py','datalogger.py','sensors.py','wlan.py','bmp280.py','cron.py','leadacid.py','picosngcja5.py','sps30.py'],
+    '/libs' : ['ahtx0.py','bmp280.py','config.py','cron.py','datalogger.py','filelogger.py','__init__.py','leadacid.py','logger.py','picosngcja5.py','sensors.py','sps30.py','wlan.py'],
     '/logs' : [],
     '/html' : ['portal.html'], 
 }
