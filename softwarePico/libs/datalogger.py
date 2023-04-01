1b7520f82e1afe4da81a8f3e082ff7a82f871e3e
Alessandro Proglio <ale.proglio@gmail.com>
2023-04-01 11:26:05 +0200

    added error handling when connection drops and datalogger can't send data

--- softwarePico/libs/datalogger.py
+++ softwarePico/libs/datalogger.py
@@ -12,6 +12,7 @@ UID = str(int(binascii.hexlify(iam).decode('utf-8'),16))
 def send_data(d):
     feed_wdt()
     gc.collect()
+    resp = None
     try:
         # if resp takes too long to arrive,
         resp = requests.post(config.datalogger['URL'], json=d, timeout=config.board['WDT_seconds']-0.5)
@@ -28,7 +29,7 @@ def send_data(d):
         # converting to integer (we assume that the server replies
         # with the new record ID if the insert succeded. An error message otherwise.
         int(resp.text)
-    except ValueError:
+    except (ValueError, AttributeError) as e:
         isInt = False
     return isInt
 
--- softwarePico/version.py
+++ softwarePico/version.py
@@ -1,9 +1,9 @@
-version = 31
+version = 33
 folders = ['/', '/libs', '/html', '/logs']
 #updates or new files. this file is manually updated and can only add or modify files or add folders
 updated_files = {
     '/'     : [], # there is no need to include version.py
-    '/libs' : ['config.py','cron.py','leadacid.py','logger.py','sensors.py','wlan.py'],
+    '/libs' : ['datalogger.py'],
     '/html' : [],
 }
 # full update requires a list of all files
