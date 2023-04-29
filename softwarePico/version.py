efe728fa522bbb21dc2a5489a105bace248e2aaf
Alessandro Proglio <ale.proglio@gmail.com>
2023-04-29 16:32:13 +0200

    version 56

--- softwarePico/version.py
+++ softwarePico/version.py
@@ -1,9 +1,9 @@
-version = 55
+version = 56
 folders = ['/', '/libs', '/html', '/logs']
 #updates or new files. this file is manually updated and can only add or modify files or add folders
 updated_files = {
     '/'     : ['main.py'], # there is no need to include version.py
-    '/libs' : ['filelogger.py'],
+    '/libs' : ['datalogger.py','filelogger.py','mqttlogger.py','picosngcja5.py','sps30.py'],
     '/html' : [],
 }
 # full update requires a list of all files
