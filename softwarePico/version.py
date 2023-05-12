8718f722d4050380c24293a74dca88fdd04ad6b5
Alessandro Proglio <ale.proglio@gmail.com>
2023-05-12 11:41:35 +0200

    Update version.py
    removed readlines and updated version to 62

--- softwarePico/version.py
+++ softwarePico/version.py
@@ -1,9 +1,9 @@
-version = 61
+version = 62
 folders = ['/', '/libs', '/html', '/logs']
 #updates or new files. this file is manually updated and can only add or modify files or add folders
 updated_files = {
     '/'     : ['main.py'], # there is no need to include version.py
-    '/libs/' : ['cron.py','datalogger.py'],
+    '/libs/' : ['cron.py','datalogger.py','filelogger.py'],
     '/html/' : [],
 }
 # full update requires a list of all files
