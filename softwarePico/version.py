58872699732915ed32a51c7958eb658768ddb20d
Alessandro Proglio <ale.proglio@gmail.com>
2023-06-14 17:39:11 +0200

    new version with working webconfig micropython server

--- softwarePico/version.py
+++ softwarePico/version.py
@@ -1,15 +1,16 @@
-version = 63
+version = 64
 folders = ['/', '/libs', '/html', '/logs']
 #updates or new files. this file is manually updated and can only add or modify files or add folders
 updated_files = {
     '/'     : ['main.py','opms.py'], # there is no need to include version.py
-    '/libs/' : ['datalogger.py','filelogger.py'],
-    '/html/' : [],
+    '/libs/' : ['ahtx0.py','bmp280.py','config.py','cron.py','datalogger.py','filelogger.py','__init__.py','leadacid.py','logger.py','microdot.mpy','mqttlogger.py','picosngcja5.py','qmc5883.py','sensors.py','simple.py','sps30.py','wlan.py'],
+    '/logs/' : [],
+    '/html/' : ['portal.html', 'portal-advanced.html', 'portal-basic.html', 'portal-datalogger.html', 'portal-mqtt.html', 'portal-network.html', 'portal-status.html', 'portal-syslogger.html'], 
 }
 # full update requires a list of all files
 all_files = {
-    '/'     : ['main.py',], # there is no need to include version.py
-    '/libs/' : ['ahtx0.py','bmp280.py','cron.py','config.py','datalogger.py','filelogger.py','__init__.py','leadacid.py','logger.py','mqttlogger.py','picosngcja5.py','qmc5883.py','sensors.py','simple.py','sps30.py','wlan.py'],
+    '/'     : ['main.py','opms.py'], # there is no need to include version.py
+    '/libs/' : ['ahtx0.py','bmp280.py','config.py','cron.py','datalogger.py','filelogger.py','__init__.py','leadacid.py','logger.py','microdot.mpy','mqttlogger.py','picosngcja5.py','qmc5883.py','sensors.py','simple.py','sps30.py','wlan.py'],
     '/logs/' : [],
-    '/html/' : ['portal.html'], 
+    '/html/' : ['portal.html', 'portal-advanced.html', 'portal-basic.html', 'portal-datalogger.html', 'portal-mqtt.html', 'portal-network.html', 'portal-status.html', 'portal-syslogger.html'], 
 }
